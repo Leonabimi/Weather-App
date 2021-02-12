da1872b14a164d7230cab656d2e3c2041bab32fc
Leonabimi <72199021+Leonabimi@users.noreply.github.com>
2021-02-12 13:03:15 +0100

    Weather

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -33,14 +33,13 @@ const App = () => {
             <sup>{weather.sys.country}</sup>
           </h2>
           <div className="city-temp">
-            {Math.round(weather.main.temp)}
+            {weather.main.temp}
             <sup>&deg;C</sup>
           </div>
           <div className="info">
             <img
               className="city-icon"
               src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt={weather.weather[0].description}
             />
             <p>{weather.weather[0].description}</p>
           </div>
